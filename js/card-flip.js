132149f369a26c0f27b54dc0e3045267694d0871
danawebbdesign <60206935+danawebbdesign@users.noreply.github.com>
2021-01-13 13:22:49 -0500

    Add files via upload

---
+++ js/card-flip.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+
+document.querySelector(".card-flip").classList.toggle("flip");
+
+
+/* EXTRA demo holder stuff
+Holder.addTheme('gray', {
+  bg: '#777',
+  fg: 'rgba(255,255,255,.75)',
+  font: 'Helvetica',
+  fontweight: 'normal'
+});
+*/
